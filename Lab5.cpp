525f2b7937ffa98038e2591c00333470afe29ad6
cottons12 <90539340+cottons12@users.noreply.github.com>
2021-10-18 16:27:22 +0800

    Add files via upload

---
+++ Lab5.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+int grade3to4(int num){
+	
+}
+int main(){
+	int grade,num;
+	cout<<"please enter your grade?";
+	cin>>grade;
+	cout<<"please enter the number of questions?";
+	cin>>num;
+	if(grade==3||grade==4){
+		grade3to4(num);
+	}
+	return 0;
+}
